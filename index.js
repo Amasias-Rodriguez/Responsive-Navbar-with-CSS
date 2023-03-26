985dfb246eef3c146a811455cb653c5813218704
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-03-25 19:32:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
 
-toggleBtn.onclick = fuction () {
+toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle('open')
     const isOpen = dropDownMenu.classList.contains('open')
 
